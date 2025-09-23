aa33da87e4d43f99cf7a6bc5dfc165eee317cd61
seancope357 <seancope357@gmail.com>
2025-09-23 09:36:50 -0500

    f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
-          defaultTheme="system"
-          storageKey="sequencehub-theme"
+          defaultTheme="light"
+          storageKey="sequencehub-theme-v2"
         >
           <AuthSessionProvider>
             {children}
